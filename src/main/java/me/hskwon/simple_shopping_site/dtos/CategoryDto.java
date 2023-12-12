174ba625d4e2a570c73dc03e96d704f5adedeb93
hoeseong <hoeseong19@naver.com>
2023-12-13 01:21:59 +0900

    feat(category): CategoryDto of method 추가

--- src/main/java/me/hskwon/simple_shopping_site/dtos/CategoryDto.java
+++ src/main/java/me/hskwon/simple_shopping_site/dtos/CategoryDto.java
@@ -1,7 +1,12 @@
 package me.hskwon.simple_shopping_site.dtos;
 
+import me.hskwon.simple_shopping_site.models.Category;
+
 public record CategoryDto(
         String id,
         String name
 ) {
+    public static CategoryDto of(Category category) {
+        return new CategoryDto(category.id().toString(), category.name());
+    }
 }
