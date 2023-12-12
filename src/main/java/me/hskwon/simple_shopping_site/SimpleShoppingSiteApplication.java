c0c4b4a0b36c869393e03e7e68658b64ff4124a1
hoeseong <hoeseong19@naver.com>
2023-12-12 23:56:29 +0900

    initial commit

---
+++ src/main/java/me/hskwon/simple_shopping_site/SimpleShoppingSiteApplication.java
@@ -0,0 +1,13 @@
+package me.hskwon.simple_shopping_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleShoppingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleShoppingSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hskwon/simple_shopping_site/SimpleShoppingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hskwon.simple_shopping_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleShoppingSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
