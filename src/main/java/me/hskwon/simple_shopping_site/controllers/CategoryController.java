6b0ac87762729e688c0937d8b60272d7e9b9c249
hoeseong <hoeseong19@naver.com>
2023-12-13 00:55:50 +0900

    refactor: categories를 변수로 분리

--- src/main/java/me/hskwon/simple_shopping_site/controllers/CategoryController.java
+++ src/main/java/me/hskwon/simple_shopping_site/controllers/CategoryController.java
@@ -1,5 +1,6 @@
 package me.hskwon.simple_shopping_site.controllers;
 
+import me.hskwon.simple_shopping_site.dtos.CategoryDto;
 import me.hskwon.simple_shopping_site.dtos.ListCategoryDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,8 @@ import java.util.List;
 public class CategoryController {
     @GetMapping()
     public ListCategoryDto getList() {
-        return new ListCategoryDto(List.of());
+        List<CategoryDto> categories = List.of();
+
+        return new ListCategoryDto(categories);
     }
 }
