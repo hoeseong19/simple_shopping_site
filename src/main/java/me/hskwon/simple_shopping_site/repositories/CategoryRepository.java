84d83a839a4ebd209b667ac69e30804d8a7635d3
hoeseong <hoeseong19@naver.com>
2023-12-13 01:42:33 +0900

    refactor(category): CategoryRepository의 finAll return type을 List<Category>로 변경

--- src/main/java/me/hskwon/simple_shopping_site/repositories/CategoryRepository.java
+++ src/main/java/me/hskwon/simple_shopping_site/repositories/CategoryRepository.java
@@ -4,5 +4,8 @@ import me.hskwon.simple_shopping_site.models.Category;
 import me.hskwon.simple_shopping_site.models.CategoryId;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends CrudRepository<Category, CategoryId> {
+    List<Category> findAll();
 }
