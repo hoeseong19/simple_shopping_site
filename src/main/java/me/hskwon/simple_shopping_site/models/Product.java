ad6d2bc4bf16cff5f76c1c778ececa8b49173d3c
hoeseong <hoeseong19@naver.com>
2023-12-17 13:45:18 +0900

    feat(product): Product에 createdAt, updatedAt 추가

--- src/main/java/me/hskwon/simple_shopping_site/models/Product.java
+++ src/main/java/me/hskwon/simple_shopping_site/models/Product.java
@@ -3,12 +3,22 @@ package me.hskwon.simple_shopping_site.models;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
 public class Product {
     @EmbeddedId
     private ProductId id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
     public ProductId id() {
         return id;
