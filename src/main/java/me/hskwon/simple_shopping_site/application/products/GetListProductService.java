aef9154c727d3c1dd778d33a6e863dce59246577
hoeseong <hoeseong19@naver.com>
2023-12-17 13:06:43 +0900

    fix(product): GetListProductService에 Service annotation 추가

--- src/main/java/me/hskwon/simple_shopping_site/application/products/GetListProductService.java
+++ src/main/java/me/hskwon/simple_shopping_site/application/products/GetListProductService.java
@@ -1,10 +1,12 @@
 package me.hskwon.simple_shopping_site.application.products;
 
 import me.hskwon.simple_shopping_site.dtos.ProductSummaryDto;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class GetListProductService {
     public List<ProductSummaryDto> getListProduct() {
         return new ArrayList<>();
