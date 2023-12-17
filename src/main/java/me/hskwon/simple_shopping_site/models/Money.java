cf76f1277ad650fe315e9d81412b2055e5b8f397
hoeseong <hoeseong19@naver.com>
2023-12-17 14:17:30 +0900

    feat: Money VO 생성

---
+++ src/main/java/me/hskwon/simple_shopping_site/models/Money.java
@@ -0,0 +1,21 @@
+package me.hskwon.simple_shopping_site.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Money {
+    @Column(name = "amount")
+    Long amount;
+
+    private Money() {
+    }
+
+    public Money(Long amount) {
+        this.amount = amount;
+    }
+
+    public Long asLong() {
+        return amount;
+    }
+}
