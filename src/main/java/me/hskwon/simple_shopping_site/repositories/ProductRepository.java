cbd80531425163ab0ff662bfbd1f612f4dd9e99f
hoeseong <hoeseong19@naver.com>
2023-12-17 12:57:46 +0900

    refactor(product): ProductRepository.findAll return type 변경

--- src/main/java/me/hskwon/simple_shopping_site/repositories/ProductRepository.java
+++ src/main/java/me/hskwon/simple_shopping_site/repositories/ProductRepository.java
@@ -4,5 +4,9 @@ import me.hskwon.simple_shopping_site.models.Product;
 import me.hskwon.simple_shopping_site.models.ProductId;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, ProductId> {
+    @Override
+    List<Product> findAll();
 }
