12f7e5909689561a83fa311dd32a310b8e3ae8f9
hoeseong <hoeseong19@naver.com>
2023-12-17 13:05:04 +0900

    feat(product): ProductSummaryDto 정적 팩토리 메서드 생성

--- src/main/java/me/hskwon/simple_shopping_site/dtos/ProductSummaryDto.java
+++ src/main/java/me/hskwon/simple_shopping_site/dtos/ProductSummaryDto.java
@@ -1,4 +1,13 @@
 package me.hskwon.simple_shopping_site.dtos;
 
-public record ProductSummaryDto() {
+import me.hskwon.simple_shopping_site.models.Product;
+
+public record ProductSummaryDto(
+        String id
+) {
+    public static ProductSummaryDto of(Product product) {
+        return new ProductSummaryDto(
+                product.id().toString()
+        );
+    }
 }
