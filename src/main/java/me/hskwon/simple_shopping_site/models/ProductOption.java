ecd99b813a0e2f5d068485e4a7a23a28d42aed43
hoeseong <hoeseong19@naver.com>
2023-12-19 23:04:11 +0900

    feat(product_option): ProductOption에 items 추가

--- src/main/java/me/hskwon/simple_shopping_site/models/ProductOption.java
+++ src/main/java/me/hskwon/simple_shopping_site/models/ProductOption.java
@@ -1,9 +1,9 @@
 package me.hskwon.simple_shopping_site.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "product_options")
@@ -14,13 +14,19 @@ public class ProductOption extends BaseEntity {
     @Column(name = "name")
     private String name;
 
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "product_option_id")
+    @OrderBy("id")
+    private List<ProductOptionItem> items = new ArrayList<>();
+
     private ProductOption() {
         super();
     }
 
-    public ProductOption(ProductOptionId id, String name) {
+    public ProductOption(ProductOptionId id, String name, List<ProductOptionItem> items) {
         this.id = id;
         this.name = name;
+        this.items = items;
     }
 
     public ProductOptionId id() {
@@ -29,5 +35,9 @@ public class ProductOption extends BaseEntity {
 
     public String name() {
         return name;
+    }
+
+    public List<ProductOptionItem> items() {
+        return items;
     }
 }
