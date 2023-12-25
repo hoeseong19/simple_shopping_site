34cccce67d5fab6e5430c279d8928d920bbd9fb2
hoeseong <hoeseong19@naver.com>
2023-12-26 01:19:42 +0900

    feat(auth): LoginService 생성. login 추가

---
+++ src/main/java/me/hskwon/simple_shopping_site/application/auth/LoginService.java
@@ -0,0 +1,10 @@
+package me.hskwon.simple_shopping_site.application.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+    public String login() {
+        return "";
+    }
+}
