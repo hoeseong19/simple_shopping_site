cf47ca54760a85d46b29e07089c051d3eb7267c5
hoeseong <hoeseong19@naver.com>
2023-12-25 22:23:55 +0900

    feat(security): WebSecurityConfig 생성

---
+++ src/main/java/me/hskwon/simple_shopping_site/security/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package me.hskwon.simple_shopping_site.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/categories").permitAll()
+                .requestMatchers(HttpMethod.GET, "/products/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/backdoor/**").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+}
