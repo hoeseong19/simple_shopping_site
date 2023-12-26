3eccf8396ccf82cec5b0c63fe0c49eff32ac7fce
hoeseong <hoeseong19@naver.com>
2023-12-27 00:35:52 +0900

    feat(auth): LoginService에 AccessTokenGenerator 주입

--- src/main/java/me/hskwon/simple_shopping_site/application/auth/LoginService.java
+++ src/main/java/me/hskwon/simple_shopping_site/application/auth/LoginService.java
@@ -1,10 +1,17 @@
 package me.hskwon.simple_shopping_site.application.auth;
 
+import me.hskwon.simple_shopping_site.utils.AccessTokenGenerator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
+    private final AccessTokenGenerator accessTokenGenerator;
+
+    public LoginService(AccessTokenGenerator accessTokenGenerator) {
+        this.accessTokenGenerator = accessTokenGenerator;
+    }
+
     public String login(String username, String password) {
-        return "";
+        return accessTokenGenerator.generate();
     }
 }
