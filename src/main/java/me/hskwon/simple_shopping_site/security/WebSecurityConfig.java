e9a9455b7f2eebce168d4a96921c36d0207c6d21
hoeseong <hoeseong19@naver.com>
2023-12-30 21:55:43 +0900

    feat(auth): PasswordEncoder 설정

--- src/main/java/me/hskwon/simple_shopping_site/security/WebSecurityConfig.java
+++ src/main/java/me/hskwon/simple_shopping_site/security/WebSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,5 +28,10 @@ public class WebSecurityConfig {
                 .anyRequest().authenticated();
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 }
