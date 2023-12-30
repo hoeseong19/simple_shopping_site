9b2af5e3456835aa1c60ba5ebeb061547943f630
hoeseong <hoeseong19@naver.com>
2023-12-30 17:14:37 +0900

    feat: tsid를 이용한 UserId 생성 기능 추가

--- src/main/java/me/hskwon/simple_shopping_site/models/UserId.java
+++ src/main/java/me/hskwon/simple_shopping_site/models/UserId.java
@@ -11,4 +11,8 @@ public class UserId extends EntityId {
     public UserId(String value) {
         super(value);
     }
+
+    public static UserId generate() {
+        return new UserId(tsid());
+    }
 }
