6fcbf158a036f3df1a018575e93426c4a3ef9492
hoeseong <hoeseong19@naver.com>
2023-12-31 02:54:54 +0900

    feat(auth): AuthUser 생성

---
+++ src/main/java/me/hskwon/simple_shopping_site/security/AuthUser.java
@@ -0,0 +1,38 @@
+package me.hskwon.simple_shopping_site.security;
+
+public record AuthUser(
+        String id,
+        String email,
+        String password,
+        String role,
+        String accessToken
+) {
+    public static AuthUser of(
+            String id,
+            String email,
+            String password,
+            String role
+    ) {
+        return new AuthUser(
+                id,
+                email,
+                password,
+                role,
+                ""
+        );
+    }
+
+    public static AuthUser authenticated(
+            String id,
+            String role,
+            String accessToken
+    ) {
+        return new AuthUser(
+                id,
+                "",
+                "",
+                role,
+                accessToken
+        );
+    }
+}
