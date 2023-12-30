86249734a7d462eec899aa0981cb04f3b7b8aa44
hoeseong <hoeseong19@naver.com>
2023-12-30 17:15:35 +0900

    feat(signup): 사용자 생성 기능 추가

--- src/main/java/me/hskwon/simple_shopping_site/application/auth/SignupService.java
+++ src/main/java/me/hskwon/simple_shopping_site/application/auth/SignupService.java
@@ -1,13 +1,24 @@
 package me.hskwon.simple_shopping_site.application.auth;
 
+import jakarta.transaction.Transactional;
+import me.hskwon.simple_shopping_site.models.User;
+import me.hskwon.simple_shopping_site.models.UserId;
 import me.hskwon.simple_shopping_site.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class SignupService {
     private final UserRepository userRepository;
 
     public SignupService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public void signup(String email, String name, String password) {
+        UserId userId = UserId.generate();
+        User newUser = new User(userId, email, name, password);
+
+        userRepository.save(newUser);
     }
 }
--- src/test/java/me/hskwon/simple_shopping_site/application/auth/SignupServiceTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/application/auth/SignupServiceTest.java
@@ -2,8 +2,12 @@ package me.hskwon.simple_shopping_site.application.auth;
 
 import me.hskwon.simple_shopping_site.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class SignupServiceTest {
     private SignupService signupService;
@@ -15,5 +19,17 @@ class SignupServiceTest {
         userRepository = mock(UserRepository.class);
 
         signupService = new SignupService(userRepository);
+    }
+
+    @Test
+    @DisplayName("signup")
+    void testSignup() {
+        String email = "a@b.c";
+        String name = "name";
+        String password = "password";
+
+        signupService.signup(email, name, password);
+
+        verify(userRepository).save(any());
     }
 }
