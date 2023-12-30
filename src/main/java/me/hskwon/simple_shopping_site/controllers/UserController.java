fbaa713727006adb20bb12ebf19c6e51e81b335b
hoeseong <hoeseong19@naver.com>
2023-12-30 17:39:38 +0900

    feat(signup): UserController에서 SignupService를 사용하여 사용자 생성

--- src/main/java/me/hskwon/simple_shopping_site/controllers/UserController.java
+++ src/main/java/me/hskwon/simple_shopping_site/controllers/UserController.java
@@ -1,17 +1,30 @@
 package me.hskwon.simple_shopping_site.controllers;
 
+import me.hskwon.simple_shopping_site.application.auth.SignupService;
+import me.hskwon.simple_shopping_site.dtos.SignupRequestDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private final SignupService signupService;
+
+    public UserController(SignupService signupService) {
+        this.signupService = signupService;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String signUp() {
+    public String signUp(
+            @RequestBody SignupRequestDto dto
+    ) {
+        signupService.signup(
+                dto.email(),
+                dto.name(),
+                dto.password()
+        );
+
         return "";
     }
 }
--- src/test/java/me/hskwon/simple_shopping_site/controllers/UserControllerTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/controllers/UserControllerTest.java
@@ -1,12 +1,16 @@
 package me.hskwon.simple_shopping_site.controllers;
 
+import me.hskwon.simple_shopping_site.application.auth.SignupService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,12 +20,31 @@ class UserControllerTest extends ControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    private SignupService signupService;
+
     @Test
     @DisplayName("POST /users")
     void testSignup() throws Exception {
-        RequestBuilder requestBuilder = post("/users");
+        String email = "a@b.c";
+        String name = "name";
+        String password = "password";
+        String json = """
+                {
+                    "email": "%s",
+                    "name": "%s",
+                    "password": "%s"
+                }
+                """.formatted(email, name, password);
+
+        RequestBuilder requestBuilder = post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
 
         mockMvc.perform(requestBuilder)
                 .andExpect(status().isCreated());
+
+        verify(signupService).signup(email, name, password);
+    }
     }
 }
