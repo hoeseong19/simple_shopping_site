f0692df58aba88c32e1f2a1a2f8e408a6daf1bcb
hoeseong <hoeseong19@naver.com>
2023-12-30 16:16:54 +0900

    feat(signup): SignupService에 UserRepository 주입

--- src/main/java/me/hskwon/simple_shopping_site/application/auth/SignupService.java
+++ src/main/java/me/hskwon/simple_shopping_site/application/auth/SignupService.java
@@ -1,7 +1,13 @@
 package me.hskwon.simple_shopping_site.application.auth;
 
+import me.hskwon.simple_shopping_site.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignupService {
+    private final UserRepository userRepository;
+
+    public SignupService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
--- src/test/java/me/hskwon/simple_shopping_site/application/auth/SignupServiceTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/application/auth/SignupServiceTest.java
@@ -1,12 +1,19 @@
 package me.hskwon.simple_shopping_site.application.auth;
 
+import me.hskwon.simple_shopping_site.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+
+import static org.mockito.Mockito.mock;
 
 class SignupServiceTest {
     private SignupService signupService;
 
+    private UserRepository userRepository;
+
     @BeforeEach
     void setUp() {
-        signupService = new SignupService();
+        userRepository = mock(UserRepository.class);
+
+        signupService = new SignupService(userRepository);
     }
 }
