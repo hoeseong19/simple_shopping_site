401f7a62835aac520b3e152ef55a7430d8d6cdcb
hoeseong <hoeseong19@naver.com>
2023-12-31 02:42:16 +0900

    feat(auth): AuthUserDao에 access_tokens 테이블에 토큰 정보를 저장하는 addAccessToken 추가

--- src/main/java/me/hskwon/simple_shopping_site/security/AuthUserDao.java
+++ src/main/java/me/hskwon/simple_shopping_site/security/AuthUserDao.java
@@ -1,15 +1,29 @@
 package me.hskwon.simple_shopping_site.security;
 
+import jakarta.transaction.Transactional;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
+@Transactional
 public class AuthUserDao {
     private final JdbcTemplate jdbcTemplate;
 
     public AuthUserDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void addAccessToken(String accessToken, String userId) {
+        String sql = """
+                INSERT INTO access_tokens (value, user_id)
+                VALUES (?, ?);
+                """;
+
+        jdbcTemplate.update(
+                sql,
+                accessToken, userId
+        );
     }
 }
