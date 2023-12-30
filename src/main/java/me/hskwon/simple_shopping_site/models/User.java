40c890b628ec4133149ac645f9196bc01f75b2ee
hoeseong <hoeseong19@naver.com>
2023-12-30 17:15:03 +0900

    feat: User에 email, name, password 추가

--- src/main/java/me/hskwon/simple_shopping_site/models/User.java
+++ src/main/java/me/hskwon/simple_shopping_site/models/User.java
@@ -1,5 +1,6 @@
 package me.hskwon.simple_shopping_site.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -10,15 +11,35 @@ public class User extends BaseEntity {
     @EmbeddedId
     private UserId id;
 
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
     private User() {
         super();
     }
 
-    public User(UserId id) {
+    public User(UserId id, String email, String name, String password) {
         this.id = id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
     }
 
     public UserId id() {
         return id;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public String name() {
+        return name;
     }
 }
