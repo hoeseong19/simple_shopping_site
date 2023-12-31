334e5fb740213394f7ba66d8a6e2bdcbec3a977e
hoeseong <hoeseong19@naver.com>
2024-01-01 06:37:07 +0900

    feat(cart): AddProductToCartService.addProduct - 장바구니에 상품 추가 기능 추가

--- src/main/java/me/hskwon/simple_shopping_site/application/cart/AddProductToCartService.java
+++ src/main/java/me/hskwon/simple_shopping_site/application/cart/AddProductToCartService.java
@@ -1,9 +1,15 @@
 package me.hskwon.simple_shopping_site.application.cart;
 
 import jakarta.transaction.Transactional;
+import me.hskwon.simple_shopping_site.models.Cart;
+import me.hskwon.simple_shopping_site.models.CartId;
+import me.hskwon.simple_shopping_site.models.ProductId;
+import me.hskwon.simple_shopping_site.models.UserId;
 import me.hskwon.simple_shopping_site.repositories.CartRepository;
 import me.hskwon.simple_shopping_site.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 @Transactional
@@ -17,5 +23,17 @@ public class AddProductToCartService {
     ) {
         this.cartRepository = cartRepository;
         this.productRepository = productRepository;
+    }
+
+    public void addProduct(ProductId productId, UserId userId) {
+        Cart cart = cartRepository.findByUserId(userId)
+                .orElse(new Cart(CartId.generate(), userId));
+
+        productRepository.findById(productId)
+                .orElseThrow(() -> new NoSuchElementException(""));
+
+        cart.addProduct(productId);
+
+        cartRepository.save(cart);
     }
 }
--- src/test/java/me/hskwon/simple_shopping_site/application/cart/AddProductToCartServiceTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/application/cart/AddProductToCartServiceTest.java
@@ -1,10 +1,21 @@
 package me.hskwon.simple_shopping_site.application.cart;
 
+import me.hskwon.simple_shopping_site.Fixtures;
+import me.hskwon.simple_shopping_site.models.Product;
+import me.hskwon.simple_shopping_site.models.UserId;
 import me.hskwon.simple_shopping_site.repositories.CartRepository;
 import me.hskwon.simple_shopping_site.repositories.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 class AddProductToCartServiceTest {
     private AddProductToCartService addProductToCartService;
@@ -20,5 +31,34 @@ class AddProductToCartServiceTest {
                 cartRepository,
                 productRepository
         );
+    }
+
+    @Test
+    @DisplayName("addProduct - with valid productId")
+    void testAddProductWithValidProductId() {
+        Product product = Fixtures.product();
+        UserId userId = new UserId("userId");
+
+        given(productRepository.findById(product.id()))
+                .willReturn(Optional.of(product));
+
+        addProductToCartService.addProduct(product.id(), userId);
+
+        verify(cartRepository).save(any());
+    }
+
+    @Test
+    @DisplayName("addProduct - with invalid productId")
+    void testAddProductWithInvalidProductId() {
+        Product product = Fixtures.product();
+        UserId userId = new UserId("userId");
+
+        given(productRepository.findById(product.id()))
+                .willReturn(Optional.empty());
+
+        assertThatThrownBy(() -> addProductToCartService.addProduct(product.id(), userId))
+                .isInstanceOf(NoSuchElementException.class);
+
+        verify(cartRepository, never()).save(any());
     }
 }
