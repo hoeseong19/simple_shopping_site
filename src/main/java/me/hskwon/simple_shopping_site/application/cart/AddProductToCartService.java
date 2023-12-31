633584a04d0276888fbabf55fa8258b366fb0c17
hoeseong <hoeseong19@naver.com>
2024-01-01 06:30:44 +0900

    feat(cart): AddProductToCartService에 CartRepository, ProductRepository 주입

--- src/main/java/me/hskwon/simple_shopping_site/application/cart/AddProductToCartService.java
+++ src/main/java/me/hskwon/simple_shopping_site/application/cart/AddProductToCartService.java
@@ -1,10 +1,21 @@
 package me.hskwon.simple_shopping_site.application.cart;
 
 import jakarta.transaction.Transactional;
+import me.hskwon.simple_shopping_site.repositories.CartRepository;
+import me.hskwon.simple_shopping_site.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 public class AddProductToCartService {
+    private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
 
+    public AddProductToCartService(
+            CartRepository cartRepository,
+            ProductRepository productRepository
+    ) {
+        this.cartRepository = cartRepository;
+        this.productRepository = productRepository;
+    }
 }
--- src/test/java/me/hskwon/simple_shopping_site/application/cart/AddProductToCartServiceTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/application/cart/AddProductToCartServiceTest.java
@@ -1,12 +1,24 @@
 package me.hskwon.simple_shopping_site.application.cart;
 
+import me.hskwon.simple_shopping_site.repositories.CartRepository;
+import me.hskwon.simple_shopping_site.repositories.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
+
+import static org.mockito.Mockito.mock;
 
 class AddProductToCartServiceTest {
     private AddProductToCartService addProductToCartService;
 
+    private CartRepository cartRepository;
+    private ProductRepository productRepository;
+
     @BeforeEach
     void setUp() {
-        addProductToCartService = new AddProductToCartService();
+        cartRepository = mock(CartRepository.class);
+        productRepository = mock(ProductRepository.class);
+        addProductToCartService = new AddProductToCartService(
+                cartRepository,
+                productRepository
+        );
     }
 }
