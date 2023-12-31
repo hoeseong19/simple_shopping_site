1988ffde7eeb19cc15334ba52c77fd15df508b76
hoeseong <hoeseong19@naver.com>
2024-01-01 04:04:01 +0900

    feat(cart): createCartLineItem - 장바구니 상품 추가 요청 api 추가

--- src/test/java/me/hskwon/simple_shopping_site/controllers/CartLineItemControllerTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/controllers/CartLineItemControllerTest.java
@@ -1,11 +1,27 @@
 package me.hskwon.simple_shopping_site.controllers;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CartLineItemController.class)
-class CartLineItemControllerTest {
+class CartLineItemControllerTest extends ControllerTest {
     @Autowired
     MockMvc mockMvc;
+
+    @Test
+    @DisplayName("POST /cart/line-items")
+    void testAddProduct() throws Exception {
+        RequestBuilder requestBuilder = post("/cart/line-items")
+                .header("Authorization", "Bearer %s".formatted(accessToken));
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isCreated());
+    }
 }
