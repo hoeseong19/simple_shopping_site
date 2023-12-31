59a9c8dcd96a8a3bbd7b6ae81af7abdfbcd9f56f
hoeseong <hoeseong19@naver.com>
2024-01-01 04:11:50 +0900

    feat(cart): AddProductToCartService 생성

---
+++ src/main/java/me/hskwon/simple_shopping_site/application/cart/AddProductToCartService.java
@@ -0,0 +1,10 @@
+package me.hskwon.simple_shopping_site.application.cart;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class AddProductToCartService {
+
+}
---
+++ src/test/java/me/hskwon/simple_shopping_site/application/cart/AddProductToCartServiceTest.java
@@ -0,0 +1,12 @@
+package me.hskwon.simple_shopping_site.application.cart;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class AddProductToCartServiceTest {
+    private AddProductToCartService addProductToCartService;
+
+    @BeforeEach
+    void setUp() {
+        addProductToCartService = new AddProductToCartService();
+    }
+}
