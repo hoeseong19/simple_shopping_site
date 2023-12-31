ebe74a4b5a332852aeac0e88373e08ab92785719
hoeseong <hoeseong19@naver.com>
2024-01-01 06:00:13 +0900

    feat(test): Fixtures에 product 추가

--- src/test/java/me/hskwon/simple_shopping_site/Fixtures.java
+++ src/test/java/me/hskwon/simple_shopping_site/Fixtures.java
@@ -1,4 +1,19 @@
 package me.hskwon.simple_shopping_site;
 
+import me.hskwon.simple_shopping_site.models.*;
+
+import java.util.List;
+
 public class Fixtures {
+    public static Product product() {
+        ProductId id = new ProductId("id");
+        CategoryId categoryId = new CategoryId("categoryId");
+        String name = "name";
+        Money price = new Money(1L);
+        List<Image> images = List.of(new Image(new ImageId("id"), "url"));
+
+        Product product = new Product(id, categoryId, name, price, images);
+
+        return product;
+    }
 }
