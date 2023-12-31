00fa7d7c3a4edde61428178d5886bfca71ab7107
hoeseong <hoeseong19@naver.com>
2024-01-01 06:39:36 +0900

    feat(cart): CartLineItemController에 AddProductToCartService 주입

--- src/main/java/me/hskwon/simple_shopping_site/controllers/CartLineItemController.java
+++ src/main/java/me/hskwon/simple_shopping_site/controllers/CartLineItemController.java
@@ -1,5 +1,6 @@
 package me.hskwon.simple_shopping_site.controllers;
 
+import me.hskwon.simple_shopping_site.application.cart.AddProductToCartService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cart/line-items")
 public class CartLineItemController {
+    private final AddProductToCartService addProductToCartService;
+
+    public CartLineItemController(AddProductToCartService addProductToCartService) {
+        this.addProductToCartService = addProductToCartService;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createCartLineItem() {
