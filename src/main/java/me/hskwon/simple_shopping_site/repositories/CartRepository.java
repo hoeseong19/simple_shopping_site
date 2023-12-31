29d6847fe8b12bca8cbef280ae43044a1a5da035
hoeseong <hoeseong19@naver.com>
2024-01-01 06:31:46 +0900

    feat(cart): CartRepository에 findByUserId 추가

--- src/main/java/me/hskwon/simple_shopping_site/repositories/CartRepository.java
+++ src/main/java/me/hskwon/simple_shopping_site/repositories/CartRepository.java
@@ -2,7 +2,11 @@ package me.hskwon.simple_shopping_site.repositories;
 
 import me.hskwon.simple_shopping_site.models.Cart;
 import me.hskwon.simple_shopping_site.models.CartId;
+import me.hskwon.simple_shopping_site.models.UserId;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends CrudRepository<Cart, CartId> {
+    Optional<Cart> findByUserId(UserId userId);
 }
