1114e37ea428c3476bbc47d695ff2cfc503ea280
hoeseong <hoeseong19@naver.com>
2024-01-01 22:54:21 +0900

    feat(cart): GetCartService 생성

---
+++ src/main/java/me/hskwon/simple_shopping_site/application/cart/GetCartService.java
@@ -0,0 +1,7 @@
+package me.hskwon.simple_shopping_site.application.cart;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetCartService {
+}
---
+++ src/test/java/me/hskwon/simple_shopping_site/application/cart/GetCartServiceTest.java
@@ -0,0 +1,12 @@
+package me.hskwon.simple_shopping_site.application.cart;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class GetCartServiceTest {
+    private GetCartService getCartService;
+
+    @BeforeEach
+    void setUp() {
+        getCartService = new GetCartService();
+    }
+}
