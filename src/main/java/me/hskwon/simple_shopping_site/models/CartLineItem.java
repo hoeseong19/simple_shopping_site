1129a212daaedd119666cf5802d5702e45d4b22a
hoeseong <hoeseong19@naver.com>
2024-01-01 19:43:43 +0900

    feat(cart): CartLineItem에 CartLineItemOption Set 추가

--- src/main/java/me/hskwon/simple_shopping_site/models/CartLineItem.java
+++ src/main/java/me/hskwon/simple_shopping_site/models/CartLineItem.java
@@ -2,6 +2,9 @@ package me.hskwon.simple_shopping_site.models;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "cart_line_items")
 public class CartLineItem extends BaseEntity {
@@ -12,13 +15,21 @@ public class CartLineItem extends BaseEntity {
     @AttributeOverride(name = "value", column = @Column(name = "product_id"))
     private ProductId productId;
 
+    @ElementCollection
+    @CollectionTable(
+            name = "cart_line_item_options",
+            joinColumns = @JoinColumn(name = "cart_line_item_id")
+    )
+    private Set<CartLineItemOption> options = new HashSet<>();
+
     private CartLineItem() {
         super();
     }
 
-    public CartLineItem(CartLineItemId id, ProductId productId) {
+    public CartLineItem(CartLineItemId id, ProductId productId, Set<CartLineItemOption> options) {
         this.id = id;
         this.productId = productId;
+        this.options = options;
     }
 
     public CartLineItemId id() {
