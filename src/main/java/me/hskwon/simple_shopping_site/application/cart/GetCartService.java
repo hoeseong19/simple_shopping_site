99f1adc54da8e12ec27814ea3d6054fb4e8b8401
hoeseong <hoeseong19@naver.com>
2024-01-01 23:03:15 +0900

    feat(cart): GetCartService에 CartRepository 주입

--- src/main/java/me/hskwon/simple_shopping_site/application/cart/GetCartService.java
+++ src/main/java/me/hskwon/simple_shopping_site/application/cart/GetCartService.java
@@ -1,7 +1,13 @@
 package me.hskwon.simple_shopping_site.application.cart;
 
+import me.hskwon.simple_shopping_site.repositories.CartRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GetCartService {
+    private final CartRepository cartRepository;
+
+    public GetCartService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
 }
--- src/test/java/me/hskwon/simple_shopping_site/application/cart/GetCartServiceTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/application/cart/GetCartServiceTest.java
@@ -1,12 +1,18 @@
 package me.hskwon.simple_shopping_site.application.cart;
 
+import me.hskwon.simple_shopping_site.repositories.CartRepository;
 import org.junit.jupiter.api.BeforeEach;
+
+import static org.mockito.Mockito.mock;
 
 class GetCartServiceTest {
     private GetCartService getCartService;
+    private CartRepository cartRepository;
 
     @BeforeEach
     void setUp() {
-        getCartService = new GetCartService();
+        cartRepository = mock(CartRepository.class);
+
+        getCartService = new GetCartService(cartRepository);
     }
 }
