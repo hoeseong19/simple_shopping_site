9919804fa109938359f2c4977f3a3529d0d1e359
hoeseong <hoeseong19@naver.com>
2024-01-01 23:12:45 +0900

    feat(cart): CartController에 GetCartService 주입

--- src/main/java/me/hskwon/simple_shopping_site/controllers/CartController.java
+++ src/main/java/me/hskwon/simple_shopping_site/controllers/CartController.java
@@ -1,5 +1,6 @@
 package me.hskwon.simple_shopping_site.controllers;
 
+import me.hskwon.simple_shopping_site.application.cart.GetCartService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cart")
 public class CartController {
+    private final GetCartService getCartService;
+
+    public CartController(GetCartService getCartService) {
+        this.getCartService = getCartService;
+    }
+
     @GetMapping
     public String getCart() {
         return "";
--- src/test/java/me/hskwon/simple_shopping_site/controllers/CartControllerTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/controllers/CartControllerTest.java
@@ -1,9 +1,11 @@
 package me.hskwon.simple_shopping_site.controllers;
 
+import me.hskwon.simple_shopping_site.application.cart.GetCartService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 
@@ -14,6 +16,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class CartControllerTest extends ControllerTest {
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    GetCartService getCartService;
 
     @Test
     @DisplayName("GET /cart")
