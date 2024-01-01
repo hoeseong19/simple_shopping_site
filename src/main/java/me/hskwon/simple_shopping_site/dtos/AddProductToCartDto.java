9e7c94ae0369e75fd24b87e30fb95d124075616f
hoeseong <hoeseong19@naver.com>
2024-01-01 19:33:19 +0900

    feat(cart): AddProductToCartDto에 optionId, optionItemId 추가

--- src/main/java/me/hskwon/simple_shopping_site/dtos/AddProductToCartDto.java
+++ src/main/java/me/hskwon/simple_shopping_site/dtos/AddProductToCartDto.java
@@ -2,8 +2,16 @@ package me.hskwon.simple_shopping_site.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.util.List;
+
 public record AddProductToCartDto(
         @NotBlank
-        String productId
+        String productId,
+        List<Option> options
 ) {
+    public record Option(
+            String id,
+            String itemId
+    ) {
+    }
 }
