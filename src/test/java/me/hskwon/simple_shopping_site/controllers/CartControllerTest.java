b81b5e40ca1150393236f3b19652b75b8740cc07
hoeseong <hoeseong19@naver.com>
2024-01-01 22:52:58 +0900

    feat(cart): 장바구니 정보 요청 api 추가

--- src/test/java/me/hskwon/simple_shopping_site/controllers/CartControllerTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/controllers/CartControllerTest.java
@@ -1,11 +1,27 @@
 package me.hskwon.simple_shopping_site.controllers;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CartController.class)
-class CartControllerTest {
+class CartControllerTest extends ControllerTest {
     @Autowired
     MockMvc mockMvc;
+
+    @Test
+    @DisplayName("GET /cart")
+    void getCart() throws Exception {
+        RequestBuilder requestBuilder = get("/cart")
+                .header("Authorization", "Bearer %s".formatted(accessToken));
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk());
+    }
 }
