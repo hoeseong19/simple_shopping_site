41aed52744ed2260e8582868d0244497e818f957
hoeseong <hoeseong19@naver.com>
2024-01-01 22:23:28 +0900

    feat(cart): AddProductToCartDto에 quantity 추가

--- src/main/java/me/hskwon/simple_shopping_site/dtos/AddProductToCartDto.java
+++ src/main/java/me/hskwon/simple_shopping_site/dtos/AddProductToCartDto.java
@@ -1,5 +1,6 @@
 package me.hskwon.simple_shopping_site.dtos;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.List;
@@ -7,7 +8,9 @@ import java.util.List;
 public record AddProductToCartDto(
         @NotBlank
         String productId,
-        List<Option> options
+        List<Option> options,
+        @Min(1)
+        int quantity
 ) {
     public record Option(
             String id,
