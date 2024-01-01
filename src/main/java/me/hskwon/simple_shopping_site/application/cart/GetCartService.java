8d5b455e7ed53273040b2cff59f581bb98896924
hoeseong <hoeseong19@naver.com>
2024-01-01 23:05:26 +0900

    feat(cart): GetCartService.getCart - Cart 조회 기능 추가

--- src/main/java/me/hskwon/simple_shopping_site/application/cart/GetCartService.java
+++ src/main/java/me/hskwon/simple_shopping_site/application/cart/GetCartService.java
@@ -1,5 +1,8 @@
 package me.hskwon.simple_shopping_site.application.cart;
 
+import me.hskwon.simple_shopping_site.models.Cart;
+import me.hskwon.simple_shopping_site.models.CartId;
+import me.hskwon.simple_shopping_site.models.UserId;
 import me.hskwon.simple_shopping_site.repositories.CartRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +12,12 @@ public class GetCartService {
 
     public GetCartService(CartRepository cartRepository) {
         this.cartRepository = cartRepository;
+    }
+
+    public Cart getCart(UserId userId) {
+        Cart cart = cartRepository.findByUserId(userId)
+                .orElse(new Cart(CartId.generate(), userId));
+
+        return cart;
     }
 }
--- src/test/java/me/hskwon/simple_shopping_site/application/cart/GetCartServiceTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/application/cart/GetCartServiceTest.java
@@ -1,9 +1,14 @@
 package me.hskwon.simple_shopping_site.application.cart;
 
+import me.hskwon.simple_shopping_site.models.Cart;
+import me.hskwon.simple_shopping_site.models.UserId;
 import me.hskwon.simple_shopping_site.repositories.CartRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class GetCartServiceTest {
     private GetCartService getCartService;
@@ -14,5 +19,16 @@ class GetCartServiceTest {
         cartRepository = mock(CartRepository.class);
 
         getCartService = new GetCartService(cartRepository);
+    }
+
+    @Test
+    void testGetCart() {
+        UserId userId = new UserId("userId");
+
+        Cart cart = getCartService.getCart(userId);
+
+        assertThat(cart).isNotNull();
+
+        verify(cartRepository).findByUserId(userId);
     }
 }
