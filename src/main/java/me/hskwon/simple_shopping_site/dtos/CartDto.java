18cc69a464e13f58a3b944d7e325a52bb3060fcd
hoeseong <hoeseong19@naver.com>
2024-01-04 23:13:58 +0900

    feat(cart): CartDto 각 하위 Dto에 정적 팩토리 메서드 생성

--- src/main/java/me/hskwon/simple_shopping_site/dtos/CartDto.java
+++ src/main/java/me/hskwon/simple_shopping_site/dtos/CartDto.java
@@ -1,5 +1,7 @@
 package me.hskwon.simple_shopping_site.dtos;
 
+import me.hskwon.simple_shopping_site.models.*;
+
 import java.util.List;
 
 public record CartDto(
@@ -14,6 +16,22 @@ public record CartDto(
             int quantity,
             long totalPrice
     ) {
+        public static LineItemDto of(CartLineItem item, Product product) {
+            List<OptionDto> options = item.optionIds().stream()
+                    .map(optionId -> {
+                        return OptionDto.of(optionId, item.optionItemId(optionId), product);
+                    })
+                    .toList();
+
+            return new LineItemDto(
+                    item.id().toString(),
+                    ProductDto.of(product),
+                    options,
+                    product.price().asLong(),
+                    item.quantity(),
+                    product.price().times(item.quantity()).asLong()
+            );
+        }
     }
 
     public record ProductDto(
@@ -21,21 +39,48 @@ public record CartDto(
             String name,
             ImageDto thumbnail
     ) {
+        public static ProductDto of(Product product) {
+            return new ProductDto(
+                    product.id().toString(),
+                    product.name(),
+                    ImageDto.of(product.image(0))
+            );
+        }
     }
 
     public record ImageDto(
             String url
     ) {
+        public static ImageDto of(Image image) {
+            return new ImageDto(image.url());
+        }
     }
 
     public record OptionDto(
             String name,
             OptionItemDto item
     ) {
+        public static OptionDto of(
+                ProductOptionId optionId,
+                ProductOptionItemId optionItemId,
+                Product product
+        ) {
+            ProductOption productOption = product.optionById(optionId);
+
+            return new OptionDto(
+                    productOption.name(),
+                    OptionItemDto.of(
+                            productOption.itemById(optionItemId)
+                    )
+            );
+        }
     }
 
     public record OptionItemDto(
             String name
     ) {
+        public static OptionItemDto of(ProductOptionItem item) {
+            return new OptionItemDto(item.name());
+        }
     }
 }
