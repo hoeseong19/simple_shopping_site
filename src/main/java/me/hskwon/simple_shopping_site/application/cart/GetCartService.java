28f509df8430e6bd340cba370093645f3c8940ee
hoeseong <hoeseong19@naver.com>
2024-01-04 23:10:11 +0900

    feat(cart): GetCartService에 ProductRepository 주입

--- src/main/java/me/hskwon/simple_shopping_site/application/cart/GetCartService.java
+++ src/main/java/me/hskwon/simple_shopping_site/application/cart/GetCartService.java
@@ -4,14 +4,17 @@ import me.hskwon.simple_shopping_site.models.Cart;
 import me.hskwon.simple_shopping_site.models.CartId;
 import me.hskwon.simple_shopping_site.models.UserId;
 import me.hskwon.simple_shopping_site.repositories.CartRepository;
+import me.hskwon.simple_shopping_site.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GetCartService {
     private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
 
-    public GetCartService(CartRepository cartRepository) {
+    public GetCartService(CartRepository cartRepository, ProductRepository productRepository) {
         this.cartRepository = cartRepository;
+        this.productRepository = productRepository;
     }
 
     public Cart getCart(UserId userId) {
--- src/test/java/me/hskwon/simple_shopping_site/application/cart/GetCartServiceTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/application/cart/GetCartServiceTest.java
@@ -3,6 +3,7 @@ package me.hskwon.simple_shopping_site.application.cart;
 import me.hskwon.simple_shopping_site.models.Cart;
 import me.hskwon.simple_shopping_site.models.UserId;
 import me.hskwon.simple_shopping_site.repositories.CartRepository;
+import me.hskwon.simple_shopping_site.repositories.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,12 +14,14 @@ import static org.mockito.Mockito.verify;
 class GetCartServiceTest {
     private GetCartService getCartService;
     private CartRepository cartRepository;
+    private ProductRepository productRepository;
 
     @BeforeEach
     void setUp() {
         cartRepository = mock(CartRepository.class);
+        productRepository = mock(ProductRepository.class);
 
-        getCartService = new GetCartService(cartRepository);
+        getCartService = new GetCartService(cartRepository, productRepository);
     }
 
     @Test
