6825fdeedbda6ea93f55e17d93afd34ba40c9603
hoeseong <hoeseong19@naver.com>
2024-01-07 20:36:53 +0900

    feat(order): CreateOrderService 생성

---
+++ src/main/java/me/hskwon/simple_shopping_site/application/orders/CreateOrderService.java
@@ -0,0 +1,9 @@
+package me.hskwon.simple_shopping_site.application.orders;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class CreateOrderService {
+}
---
+++ src/test/java/me/hskwon/simple_shopping_site/application/orders/CreateOrderServiceTest.java
@@ -0,0 +1,12 @@
+package me.hskwon.simple_shopping_site.application.orders;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class CreateOrderServiceTest {
+    private CreateOrderService createOrderService;
+
+    @BeforeEach
+    void setUp() {
+        createOrderService = new CreateOrderService();
+    }
+}
