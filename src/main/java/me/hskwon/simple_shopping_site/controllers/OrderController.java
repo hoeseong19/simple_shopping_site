f36d984f4e47f486ad0c17f1bb2ae5f5e2836d7a
hoeseong <hoeseong19@naver.com>
2024-01-07 20:30:44 +0900

    feat(order): OrderController 생성

---
+++ src/main/java/me/hskwon/simple_shopping_site/controllers/OrderController.java
@@ -0,0 +1,10 @@
+package me.hskwon.simple_shopping_site.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    
+}
