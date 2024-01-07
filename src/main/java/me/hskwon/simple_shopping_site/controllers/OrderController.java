01ba12e7c529bfa10bc949cfd510d13d6297047d
hoeseong <hoeseong19@naver.com>
2024-01-07 20:34:44 +0900

    feat(order): 주문 생성 api 추가

--- src/main/java/me/hskwon/simple_shopping_site/controllers/OrderController.java
+++ src/main/java/me/hskwon/simple_shopping_site/controllers/OrderController.java
@@ -1,10 +1,16 @@
 package me.hskwon.simple_shopping_site.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
-    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createOrder() {
+    }
 }
--- src/test/java/me/hskwon/simple_shopping_site/controllers/OrderControllerTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/controllers/OrderControllerTest.java
@@ -1,11 +1,27 @@
 package me.hskwon.simple_shopping_site.controllers;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(OrderController.class)
 class OrderControllerTest extends ControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("POST /orders")
+    void testCreateOrder() throws Exception {
+        RequestBuilder requestBuilder = post("/orders")
+                .header("Authorization", "Bearer %s".formatted(accessToken));
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isCreated());
+    }
 }
