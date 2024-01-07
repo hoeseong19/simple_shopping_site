c4a2d93d3afe396199e3c909e2ca33c87fe8034d
hoeseong <hoeseong19@naver.com>
2024-01-07 21:48:02 +0900

    feat(order): OrderController에 CreateOrderService 주입

--- src/main/java/me/hskwon/simple_shopping_site/controllers/OrderController.java
+++ src/main/java/me/hskwon/simple_shopping_site/controllers/OrderController.java
@@ -1,5 +1,6 @@
 package me.hskwon.simple_shopping_site.controllers;
 
+import me.hskwon.simple_shopping_site.application.orders.CreateOrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+    private final CreateOrderService createOrderService;
+
+    public OrderController(CreateOrderService createOrderService) {
+        this.createOrderService = createOrderService;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createOrder() {
