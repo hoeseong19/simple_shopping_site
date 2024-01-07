36a3608d7367d39134ddc669eb04f329b4906345
hoeseong <hoeseong19@naver.com>
2024-01-07 21:25:08 +0900

    feat(order): CreateOrderService에 OrderRepository 주입

--- src/main/java/me/hskwon/simple_shopping_site/application/orders/CreateOrderService.java
+++ src/main/java/me/hskwon/simple_shopping_site/application/orders/CreateOrderService.java
@@ -1,11 +1,18 @@
 package me.hskwon.simple_shopping_site.application.orders;
 
 import jakarta.transaction.Transactional;
+import me.hskwon.simple_shopping_site.repositories.OrderRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 public class CreateOrderService {
+    private final OrderRepository orderRepository;
+
+    public CreateOrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     public void createOrder() {
     }
 }
--- src/test/java/me/hskwon/simple_shopping_site/application/orders/CreateOrderServiceTest.java
+++ src/test/java/me/hskwon/simple_shopping_site/application/orders/CreateOrderServiceTest.java
@@ -1,14 +1,19 @@
 package me.hskwon.simple_shopping_site.application.orders;
 
+import me.hskwon.simple_shopping_site.repositories.OrderRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.mock;
+
 class CreateOrderServiceTest {
     private CreateOrderService createOrderService;
+    private OrderRepository orderRepository;
 
     @BeforeEach
     void setUp() {
-        createOrderService = new CreateOrderService();
+        orderRepository = mock(OrderRepository.class);
+        createOrderService = new CreateOrderService(orderRepository);
     }
 
     @Test
