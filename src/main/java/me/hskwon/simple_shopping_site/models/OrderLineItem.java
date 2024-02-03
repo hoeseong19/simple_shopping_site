548992d47e0bebda2b7e3e70d25b3744c055b718
hoeseong <hoeseong19@naver.com>
2024-02-03 23:28:39 +0900

    feat(order): OrderLineItem에 OrderOption 리스트 속성 추가

--- src/main/java/me/hskwon/simple_shopping_site/models/OrderLineItem.java
+++ src/main/java/me/hskwon/simple_shopping_site/models/OrderLineItem.java
@@ -1,15 +1,31 @@
 package me.hskwon.simple_shopping_site.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "order_line_items")
 public class OrderLineItem extends BaseEntity<OrderLineItemId> {
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "order_line_item_id")
+    @OrderBy("id")
+    private List<OrderOption> options = new ArrayList<>();
+
     private OrderLineItem() {
     }
 
-    public OrderLineItem(OrderLineItemId id) {
+    public OrderLineItem(OrderLineItemId id, List<OrderOption> options) {
         super(id);
+        this.options = options;
+    }
+
+    public int optionSize() {
+        return options.size();
+    }
+
+    public OrderOption option(int index) {
+        return options.get(index);
     }
 }
